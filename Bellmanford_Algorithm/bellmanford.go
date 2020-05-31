7fe9ac5d9bc50e7a9bc2b89119b62985597cdf57
Ujjwal Tiwari <43015170+ujjwaltiwari13@users.noreply.github.com>
2020-05-31 20:44:08 +0530

    Changed the whole bellman ford code

--- Bellmanford_Algorithm/bellmanford.go
+++ Bellmanford_Algorithm/bellmanford.go
@@ -1,130 +1,64 @@
 package main
-
 import (
-	"bufio"
-	"flag"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+        "fmt"
+        "math"
 )
 
-var (
-	input = flag.String("i", "test_set.txt", "input filename")
-)
-
-const (
-	maxint = 1<<63 - 1
-)
-
-func smallestPath(a [][]int, n int) (s int, cycle bool) {
-	for k := 0; k < n; k++ {
-		a = nextK(a, n, k)
-	}
-	cycle = detectCycle(a, n)
-	s = minVal(a, n)
-	return
+func main(){
+        var times =[][]int{ {1,2,1},{2,3,1},{3,4,1}}
+        var N, K,g int 
+        N = 4 
+        K = 1 
+        V :=3 
+        E :=3 
+        g= BellmanFord(times,N,K,V,E)
+        fmt.Printf("Steps needed to reach the destination from vertex %d to vertex %d is %d.\n\n",K,N,g)
+        if detectCycle(times, N) {
+                fmt.Println("There was a negative cost cycle detected." )
+        }else{
+                fmt.Println("There was no cycle detected");
+        }
 }
 
-func minInt(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+func detectCycle(a [][]int, N int) bool {
+        for i := 0; i < len(a); i++ {
+                //for j:=0; j< len(a[i]);j++){
+                if a[i][i] < 0 { 
+                        return true
+                }
+        }
+        return false
 }
 
-func minVal(a [][]int, n int) int {
-	r := maxint
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			if i != j && a[i][j] < r {
-				r = a[i][j]
-			}
-		}
-	}
-	return r
-}
-
-func detectCycle(a [][]int, n int) bool {
-	for i := 0; i < n; i++ {
-		if a[i][i] < 0 {
-			return true
-		}
-	}
-	return false
-}
-
-func nextK(a [][]int, n, k int) [][]int {
-	r := make([][]int, n)
-	for i := 0; i < n; i++ {
-		r[i] = make([]int, n)
-	}
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			// case 1
-			c1 := a[i][j]
-			// case 2
-			p := a[i][k]
-			q := a[k][j]
-			var c2 int
-			if p == maxint || q == maxint {
-				c2 = maxint
-			} else {
-				c2 = p + q
-			}
-			// set min of each case
-			r[i][j] = minInt(c1, c2)
-		}
-	}
-	return r
-}
-
-func main() {
-	flag.Parse()
-	f, err := os.Open(*input)
-	if err != nil {
-		panic(err)
-	}
-	scanner := bufio.NewScanner(f)
-	scanner.Scan()
-	params := scanner.Text()
-	p := strings.Fields(params)
-	n, _ := strconv.Atoi(p[0])
-	// m, _ := strconv.Atoi(p[1])
-
-	a := make([][]int, n)
-	for x := range a {
-		a[x] = make([]int, n)
-		for y := range a[x] {
-			if x == y {
-				a[x][y] = 0
-			} else {
-				a[x][y] = maxint
-			}
-		}
-	}
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		fields := strings.Fields(line)
-		u, err := strconv.Atoi(fields[0])
-		if err != nil {
-			panic(err)
-		}
-		v, err := strconv.Atoi(fields[1])
-		if err != nil {
-			panic(err)
-		}
-
-		c, err := strconv.Atoi(fields[2])
-		if err != nil {
-			panic(err)
-		}
-		a[u-1][v-1] = c
-	}
-	minC, cycle := smallestPath(a, n)
-	log.Printf("The Shortest distance from src %d\n", minC)
-	if cycle {
-		log.Println("There was a negative cost cycle detected.")
-	}
+func BellmanFord(times [][]int, N int, K int, V int, E int) int {
+    dist := make([]int, N+1)
+    for i, _ := range dist {
+        dist[i] = math.MaxInt32
+    }   
+    dist[K] = 0 
+    for i := 0; i < V ; i++ {
+            for j:= 0; j < E; j++ {
+            if dist[times[j][0]] + times[j][2] < dist[times[j][1]] {
+                dist[times[j][1]] =dist[times[j][0]] + times[j][2]
+                }
+        }
+    }   
+    fmt.Println("Vertex \t\t Distance from Source")
+    for i :=1; i<=N; i++ {
+        fmt.Printf("%d \t\t %d\n",i,dist[i]);
+    }   
+    fmt.Println()
+    
+    maxWait := 0
+    
+    for x := 1; x <= N; x++ {
+        maxWait = int(math.Max(float64(maxWait), float64(dist[x])))
+    }   
+    
+    if maxWait != math.MaxInt32 {
+        return maxWait
+    } else {
+        return -1
+    }   
+    
 }
